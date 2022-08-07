5ac7c7c9131abc19e8299c7664caf7bf932f65e6
github_Leomary1602 <marycolina@gmail.com>
2022-08-06 21:20:07 -0400

    Agregando archivo de metodo numero

---
+++ metodos_numeros.rb
@@ -0,0 +1,20 @@
+numero_entero = 1776
+#metodos para numeros enteros 
+puts numero_entero.next
+puts numero_siguiente = numero_entero.succ
+#metodo para validar si un numero es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+#conversion de entero a flotante
+puts numero_siguiente / numero_entero.to_f
+puts 800 / 270.to_f
+#poner en digitos un numero
+print 356.digits
+print 78905.digits
+#redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
